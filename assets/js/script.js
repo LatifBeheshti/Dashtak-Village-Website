d48c1a6b5d564642060e43fcfa3e6b868ce774bb
latifbeheshti <ab.latifbeheshti@gmail.com>
2025-02-02 23:56:00 +0100

    - Font Awsome for Home Button and Contact Section Updated. - Scroll to Top Button added to the webpage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -490,3 +490,16 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+var rootElement = document.documentElement;
+
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
